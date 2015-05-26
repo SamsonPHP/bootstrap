99794b6e3cb1ab8c38888a0fffa6c13fc7d46970
Vitaly Iegorov <egorov@samsonos.com>
2015-05-26 13:24:17 +0300

    Added comments

--- src/main/src/Controller.php
+++ src/main/src/Controller.php
@@ -1,8 +1,15 @@
 <?php
-namespace htmlapp\main;
+namespace samsonphp\main;
 
-class Controller extends \samson\core\CompressableExternalModule
+use samson\core\CompressableExternalModule;
+
+/**
+ * Module controller
+ * @package samsonphp\main
+ */
+class Controller extends CompressableExternalModule
 {
+    /** @var string Identifier */
 	protected $id = 'main';
 
     /** Universal main controller action */
